e5da4b58c88046ab5ff36124bd16f17d31be08dc
liulin <11371564@qq.com>
2025-02-10 14:46:17 +0800

    feat: 答案之书、舔狗日记服务调整

--- Base/src/main/java/com/bot/base/service/impl/AnswerBookServiceImpl.java
+++ Base/src/main/java/com/bot/base/service/impl/AnswerBookServiceImpl.java
@@ -1,11 +1,16 @@
 package com.bot.base.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.bot.base.dto.CommonResp;
 import com.bot.base.service.BaseService;
 import com.bot.common.enums.ENRespType;
 import com.bot.common.loader.CommonTextLoader;
 import com.bot.common.constant.BaseConsts;
+import com.bot.common.util.HttpSenderUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 
@@ -17,9 +22,18 @@ import org.springframework.stereotype.Service;
 @Service(BaseConsts.ClassBeanName.ANSWER_BOOK)
 public class AnswerBookServiceImpl implements BaseService {
 
+    @Value("${answer.book.url}")
+    private String url;
+
     @Override
     public CommonResp doQueryReturn(String reqContent, String token) {
-        int index = RandomUtil.randomInt(0, CommonTextLoader.answers.size());
-        return new CommonResp(CommonTextLoader.answers.get(index), ENRespType.TEXT.getType());
+        String extContent = reqContent.replaceAll(BaseConsts.Menu.ANSWER_BOOK, "").trim();
+        if (StrUtil.isEmpty(extContent)) {
+            return new CommonResp("请带上你的问题再使用答案之书", ENRespType.TEXT.getType());
+        }
+        String resultObj = HttpSenderUtil.get(url + "?question=" + extContent, null);
+        JSONObject data = (JSONObject) JSONUtil.parseObj(resultObj).get("data");
+        String result = (String) data.get("description_zh");
+        return new CommonResp(result, ENRespType.TEXT.getType());
     }
 }
